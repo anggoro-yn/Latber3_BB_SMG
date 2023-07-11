8969fe477a0c199e6736bc45b3b5775ae2e34a7f
Anggoro Yudho N <70189451+anggoro-yn@users.noreply.github.com>
2023-07-11 12:16:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,16 @@ np.random.randn(20, 5),
 columns=('col_no %d' % i for i in range(5)))
 st.dataframe(df)
 
+
+# https://docs.google.com/spreadsheets/d/1W2smfo-KRBcU2DMM7tjwaW8BU5Ls2P7QvTmqoj_3slE/edit?usp=sharing
+SHEET_ID = '1W2smfo-KRBcU2DMM7tjwaW8BU5Ls2P7QvTmqoj_3slE'
+SHEET_NAME = 'Peringkat'
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+df = pd.read_csv(url)
+print(df.head())
+st.dataframe(df)
+
+
 # defining random values in a dataframe using pandas and numpy
 df = pd.DataFrame(
 np.random.randn(20, 5),
