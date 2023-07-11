bc23b68beee0d0fecee87df265a8879fd6712e17
Anggoro Yudho N <70189451+anggoro-yn@users.noreply.github.com>
2023-07-11 14:19:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ SHEET_NAME2 = 'PeringkatPA'
 url1 = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME1}'
 url2 = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME2}'
 colnames = ['Peringkat', 'Target', 'Nama', 'Klub','Total','S1-1','S1-2','S1-3','S1-4','S1-5','S1-6','Tot S1','S2-1','S2-2','S2-3','S2-4','S2-5','S2-6','Tot S2']
+'''
 df1 = pd.read_csv(url1, names = colnames)
 st.write(df1.head())
 st.dataframe(df1)
@@ -29,7 +30,7 @@ st.dataframe(df1)
 df2 = pd.read_csv(url2, names = colnames)
 st.write(df2.head())
 st.dataframe(df2)
-
+'''
 
 @st.cache(ttl=seconds)
 def get_data(url):
